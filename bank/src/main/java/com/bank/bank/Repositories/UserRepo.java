a8a983b0d8ff93140c5ee98e2d627de33187fef5
ahmedemad2222 <86380434+ahmedemad2222@users.noreply.github.com>
2023-04-27 22:48:28 +0200

    fix2

--- bank/src/main/java/com/bank/bank/Repositories/UserRepo.java
+++ bank/src/main/java/com/bank/bank/Repositories/UserRepo.java
@@ -3,7 +3,8 @@ package com.bank.bank.Repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bank.bank.Models.Manager;
+import com.bank.bank.Models.User;
 
-public interface UserRepo extends JpaRepository<Manager, Long>{
+public interface UserRepo extends JpaRepository<User, Long>{
     
 }
