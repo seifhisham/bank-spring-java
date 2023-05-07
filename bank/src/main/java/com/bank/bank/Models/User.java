4729cec95eb791149ed6e539c7e53dda0bccc0c1
ahmedemad2222 <86380434+ahmedemad2222@users.noreply.github.com>
2023-05-07 23:23:33 +0300

    edit 1

--- bank/src/main/java/com/bank/bank/Models/User.java
+++ bank/src/main/java/com/bank/bank/Models/User.java
@@ -1,13 +1,17 @@
 package com.bank.bank.Models;
 import javax.persistence.*;
 
+import org.hibernate.annotations.GenericGenerator;
+
 // User entity
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 public class User {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    protected int id;
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    protected String id;
 
     protected String name;
 
@@ -15,22 +19,29 @@ public class User {
 
     protected String phoneNumber;
 
+    private String email;
 
-    public User(int id, String name, String address, String phoneNumber) {
+    private String password;
+
+
+    public User(String id, String name, String address, String phoneNumber, String email, String password) {
         this.id = id;
         this.name = name;
         this.address = address;
         this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.password = password;
     }
+
 
     public User() {
     }
 
-    public int getId() {
+    public String getId() {
         return this.id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
@@ -56,6 +67,23 @@ public class User {
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
 }
