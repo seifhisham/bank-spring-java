472720e7da9e51c24afa2fb12581f85c6bf0e2b7
Seif Hisham <74967136+seifhisham@users.noreply.github.com>
2023-05-12 00:41:17 +0300

    Update Employee.java

--- bank/src/main/java/com/bank/bank/Models/Employee.java
+++ bank/src/main/java/com/bank/bank/Models/Employee.java
@@ -5,10 +5,9 @@ import javax.persistence.*;
 @Entity
 public class Employee extends User {
 
-    public Employee(String id, String name, String address, String phoneNumber, String email, String password,
-            String Role) {
-        super(id, name, address, phoneNumber, email, password, Role);
-        // TODO Auto-generated constructor stub
+    public Employee(String id, String name, String address, String phoneNumber,String email, String password) {
+        super(id, name, address, phoneNumber,email,password);
+        //TODO Auto-generated constructor stub
     }
 
     // TODO Auto-generated constructor stub
