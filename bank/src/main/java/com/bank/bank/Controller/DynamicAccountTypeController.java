ed405184e3da8d17a188327a4447581ddee916fd
Mohamed-Said12 <99090723+Mohamed-Said12@users.noreply.github.com>
2023-05-13 02:00:55 +0300

    Account type Controller

--- bank/src/main/java/com/bank/bank/Controller/DynamicAccountTypeController.java
+++ bank/src/main/java/com/bank/bank/Controller/DynamicAccountTypeController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.bank.bank.Models.Account;
 import com.bank.bank.Models.AccountType;
 import com.bank.bank.Models.User;
 import com.bank.bank.Repositories.AccountTypeRepo;
@@ -17,16 +19,47 @@ import com.bank.bank.Repositories.AccountTypeRepo;
 @Controller
 @RequestMapping("/thymeleaf")
 public class DynamicAccountTypeController {
-    
+
     @Autowired
     private AccountTypeRepo accountTypeRepo;
 
     @GetMapping("View-accounttype")
-    public ModelAndView getPost() {
+    public ModelAndView getAccountList() {
         ModelAndView mav = new ModelAndView("AccountType.html");
 
         List<AccountType> accountList = accountTypeRepo.findAll();
-        mav.addObject("users", accountList);
+        mav.addObject("accountTypes", accountList);
+        return mav;
+    }
+
+    @GetMapping("add-account-type")
+    public ModelAndView getAddPostForm() {
+        ModelAndView mav = new ModelAndView("AddType.html");
+        AccountType accountType = new AccountType();
+        mav.addObject("accountTypes", accountType);
+        return mav;
+    }
+
+    @PostMapping("save-accounttype")
+    public String savePost(@ModelAttribute AccountType accountType) {
+
+        this.accountTypeRepo.save(accountType);
+
+        return "redirect:/thymeleaf/View-accounttype";
+
+    }
+
+    @GetMapping("delete-account-type")
+    public String deletePost(@RequestParam("Id") Long Id) {
+        this.accountTypeRepo.deleteById(Id);
+        return "redirect:/thymeleaf/View-accounttype";
+    }
+
+    @GetMapping("update-account-type")
+    public ModelAndView getUpdatePostForm(@RequestParam("Id") Long Id) {
+        ModelAndView mav = new ModelAndView("Addaccount.html");
+        AccountType accountType = this.accountTypeRepo.findById(Id).orElse(null);
+        mav.addObject("accountTypes", accountType);
         return mav;
     }
 
