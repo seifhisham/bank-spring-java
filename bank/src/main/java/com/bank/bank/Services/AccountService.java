ad5942a88a96b478cd44a2e7e3b1efcdad9636d1
Mohamed-Said12 <99090723+Mohamed-Said12@users.noreply.github.com>
2023-05-14 23:19:57 +0300

    Fixing adding account

---
+++ bank/src/main/java/com/bank/bank/Services/AccountService.java
@@ -0,0 +1,43 @@
+package com.bank.bank.Services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bank.bank.Models.Account;
+import com.bank.bank.Models.AccountType;
+import com.bank.bank.Models.User;
+import com.bank.bank.Repositories.AccountRepo;
+import com.bank.bank.Repositories.AccountTypeRepo;
+import com.bank.bank.Repositories.UserRepo;
+
+@Service
+public class AccountService {
+
+    @Autowired
+    private AccountRepo accountRepository;
+
+    @Autowired
+    private AccountTypeRepo accountTypeRepository;
+    @Autowired
+    private UserRepo userRepo;
+
+    public void saveAccount(Long typeId, double balance, String userId) {
+        Account account = new Account();
+        User user = new User();
+        user = userRepo.findById(userId).orElse(null);
+        account.setUser(user);
+
+        account.setUser(user);
+
+        AccountType accountType = accountTypeRepository.findById(typeId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid account type id: " + typeId));
+        account.setAccounttype(accountType);
+
+        account.setBalance(balance);
+
+        accountRepository.save(account);
+    }
+
+}
