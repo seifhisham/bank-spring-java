725706be3335e217b29064b306edf1be9a3e3c4a
ahmedemad2222 <86380434+ahmedemad2222@users.noreply.github.com>
2023-05-15 17:40:20 +0300

    withdraw deposit service edit

--- bank/src/main/java/com/bank/bank/Services/WithdrawDepositService.java
+++ bank/src/main/java/com/bank/bank/Services/WithdrawDepositService.java
@@ -5,32 +5,35 @@ import org.springframework.stereotype.Service;
 
 import com.bank.bank.Models.Account;
 import com.bank.bank.Models.Withdraw_Deposit;
-import com.bank.bank.Models.Transaction.TransactionType;
 import com.bank.bank.Repositories.AccountRepo;
 import com.bank.bank.Repositories.WithdrawRepo;
 
 @Service
 public class WithdrawDepositService {
+
     @Autowired
     private AccountRepo accountRepository;
 
     @Autowired
     private WithdrawRepo withdrawRepo;
 
-    public void SaveWithdraw(double amount, String date, TransactionType transactionType, Long RelatedAccount_ID) {
+    public void SaveWithdraw(double amount, String date, Withdraw_Deposit.Type type, Long relatedAccountID) {
         Withdraw_Deposit withdraw = new Withdraw_Deposit();
-        Account account = accountRepository.findById(RelatedAccount_ID)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid account  id: " + RelatedAccount_ID));
+        Account account = accountRepository.findById(relatedAccountID)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid account ID: " + relatedAccountID));
 
         withdraw.setRelatedAccount(account);
         withdraw.setAmount(amount);
         withdraw.setDate(date);
-        withdraw.setTransactionType(transactionType);
+        withdraw.setType(type);
         withdrawRepo.save(withdraw);
 
-        account.setBalance(account.getBalance() - amount);
+        if (type == Withdraw_Deposit.Type.WITHDRAW) {
+            account.setBalance(account.getBalance() - amount);
+        } else if (type == Withdraw_Deposit.Type.DEPOSIT) {
+            account.setBalance(account.getBalance() + amount);
+        }
+
         accountRepository.save(account);
-
     }
-
 }
