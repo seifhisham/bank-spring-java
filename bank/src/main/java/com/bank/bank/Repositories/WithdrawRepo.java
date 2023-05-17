09aab2f23807f448530147516118eb15dff86fd2
ahmedemad2222 <86380434+ahmedemad2222@users.noreply.github.com>
2023-05-18 01:59:28 +0300

    edit repo

--- bank/src/main/java/com/bank/bank/Repositories/WithdrawRepo.java
+++ bank/src/main/java/com/bank/bank/Repositories/WithdrawRepo.java
@@ -1,10 +1,13 @@
 package com.bank.bank.Repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bank.bank.Models.User;
 import com.bank.bank.Models.Withdraw_Deposit;
 
 public interface WithdrawRepo extends JpaRepository<Withdraw_Deposit, Long> {
-
+    List<Withdraw_Deposit> findAllByRelatedAccountUser(User user);
 }
+
