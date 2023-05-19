9b22eed5d6276cd823ef4781388fd4c555e2932e
Mohamed-Said12 <99090723+Mohamed-Said12@users.noreply.github.com>
2023-05-19 20:25:14 +0300

    delete manager repo

--- bank/src/main/java/com/bank/bank/Repositories/UserRepo.java
+++ bank/src/main/java/com/bank/bank/Repositories/UserRepo.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.bank.bank.Models.Manager;
 import com.bank.bank.Models.User;
 
 public interface UserRepo extends JpaRepository<User, String> {
