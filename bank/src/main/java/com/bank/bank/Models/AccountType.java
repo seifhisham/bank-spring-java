86c35fa0ebbe0239f4dce2e2a1ce8cc7ba26bb6b
marwanf8 <marwanfathy86@gmail.com>
2023-05-21 01:07:28 +0300

    Delete line

--- bank/src/main/java/com/bank/bank/Models/AccountType.java
+++ bank/src/main/java/com/bank/bank/Models/AccountType.java
@@ -1,12 +1,10 @@
 package com.bank.bank.Models;
 
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.Objects;
 
 @Entity
 public class AccountType {
@@ -68,9 +66,6 @@ public class AccountType {
         return this;
     }
 
-
-    
-  
 }
 
 
