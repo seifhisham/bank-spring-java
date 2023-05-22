b2b3b8e2e7b94e76f301b8c5ee9ef4ed5faae017
ahmedemad2222 <86380434+ahmedemad2222@users.noreply.github.com>
2023-05-22 03:04:24 +0300

    dynamic account controller

--- bank/src/main/java/com/bank/bank/Controller/DynamicAccountController.java
+++ bank/src/main/java/com/bank/bank/Controller/DynamicAccountController.java
@@ -1,9 +1,9 @@
 package com.bank.bank.Controller;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import com.bank.bank.Models.User;
 import com.bank.bank.Models.Account;
-
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.bank.bank.Repositories.AccountRepo;
 import com.bank.bank.Repositories.AccountTypeRepo;
 import com.bank.bank.Repositories.UserRepo;
