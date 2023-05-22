bc2cd75cc6e6ca086449f4031dd22a72975fa6fb
ahmedemad2222 <86380434+ahmedemad2222@users.noreply.github.com>
2023-05-22 16:46:27 +0300

    edit withdraw aop

--- bank/src/main/java/com/bank/bank/Controller/DynamicWithdrawDepositController.java
+++ bank/src/main/java/com/bank/bank/Controller/DynamicWithdrawDepositController.java
@@ -1,8 +1,7 @@
 package com.bank.bank.Controller;
-import java.net.http.HttpRequest;
+
 import java.time.LocalDate;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.bank.bank.Models.User;
 import com.bank.bank.Models.Withdraw_Deposit;
 import com.bank.bank.Models.Transaction.TransactionType;
