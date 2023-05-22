4f3a80d9f6cea6c80522fce4b2e22b456ea9c29a
ahmedemad2222 <86380434+ahmedemad2222@users.noreply.github.com>
2023-05-22 17:28:30 +0300

    edit account controller

--- bank/src/main/java/com/bank/bank/Controller/DynamicAccountTypeController.java
+++ bank/src/main/java/com/bank/bank/Controller/DynamicAccountTypeController.java
@@ -1,7 +1,6 @@
 package com.bank.bank.Controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
