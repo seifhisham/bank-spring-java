3f9c9811abb7badccee5e16927d78cc75ad73b01
ahmedemad2222 <86380434+ahmedemad2222@users.noreply.github.com>
2023-05-26 00:24:51 +0300

    edit dynamic withdra

--- bank/src/main/java/com/bank/bank/Controller/DynamicWithdrawDepositController.java
+++ bank/src/main/java/com/bank/bank/Controller/DynamicWithdrawDepositController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import java.time.LocalDate;
 import com.bank.bank.Models.User;
 import com.bank.bank.Models.Withdraw_Deposit;
 import com.bank.bank.Models.Transaction.TransactionType;
