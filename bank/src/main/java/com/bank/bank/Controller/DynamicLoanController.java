786214ad3e6ac375712fc669148b39928c64e753
ahmedemad2222 <86380434+ahmedemad2222@users.noreply.github.com>
2023-05-26 00:24:20 +0300

    edit dynamic loan

--- bank/src/main/java/com/bank/bank/Controller/DynamicLoanController.java
+++ bank/src/main/java/com/bank/bank/Controller/DynamicLoanController.java
@@ -3,7 +3,10 @@ package com.bank.bank.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,68 +17,74 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.bank.bank.Models.Account;
 import com.bank.bank.Models.Loan;
-import com.bank.bank.Models.User;
 import com.bank.bank.Models.Loan.LoanStatus;
 import com.bank.bank.Repositories.AccountRepo;
-import com.bank.bank.Repositories.LoanRepo;
+import com.bank.bank.Models.User;
 import com.bank.bank.Services.LoanService;
 
 @Controller
 @RequestMapping("/thymeleaf")
 public class DynamicLoanController {
-    @Autowired
-    private LoanRepo loanRepo;
-
-    @Autowired
-    private AccountRepo accountRepo;
 
     @Autowired
     private LoanService loanService;
 
+    @Autowired
+    private AccountRepo accountRepo;
+
     @GetMapping("View-Loan-Id")
-    public ModelAndView GetLoansListID() {
+    public ModelAndView getLoansListID() {
         ModelAndView mav = new ModelAndView("ViewLoans.html");
 
         // Get the logged-in user
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User user = (User) authentication.getPrincipal();
 
-        // Retrieve the transfers for the logged-in user
-        List<Loan> LoanList = loanRepo.findAllByRelatedAccountUser(user);
+        // Retrieve the loans for the logged-in user
+        List<Loan> loanList = loanService.findAllByRelatedAccountUser(user);
 
-        mav.addObject("Loans", LoanList);
+        mav.addObject("loans", loanList);
         return mav;
     }
 
     @GetMapping("View-Loan")
-    public ModelAndView GetLoansList() {
+    public ModelAndView getLoansList() {
         ModelAndView mav = new ModelAndView("ViewAllLoans.html");
-        List<Loan> LoanList = loanRepo.findAll();
-        mav.addObject("Loans", LoanList);
+        List<Loan> loanList = loanService.findAll();
+        mav.addObject("loans", loanList);
 
         return mav;
     }
 
     @GetMapping("Add-Loan")
-    public ModelAndView getAddPostForm() {
+    public ModelAndView getAddLoanForm(@AuthenticationPrincipal User user) {
         ModelAndView mav = new ModelAndView("AddLoan.html");
+       
         Loan loan = new Loan();
 
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = (User) authentication.getPrincipal();
+        mav.addObject("loan", loan);
         mav.addObject("accountList", accountRepo.findAllByUser(user));
-        mav.addObject("Loans", loan);
         return mav;
     }
 
     @PostMapping("/Save-Loan")
-    public String SaveLoan(
+    public ResponseEntity<String> saveLoan(
             @RequestParam("accountId") Long accountId,
             @RequestParam("amount") double amount) {
-
-        loanService.SaveLoan(amount, accountId, LoanStatus.WAITING);
-
-        return "redirect:/thymeleaf/View-Loan-Id";
+    
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
+    
+        Loan savedLoan = loanService.SaveLoan(amount, accountId, LoanStatus.WAITING);
+    
+        if (savedLoan != null) {
+            return ResponseEntity.ok().body("Loan saved successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to save loan");
+        }
     }
+    
 
+    
+    
 }
