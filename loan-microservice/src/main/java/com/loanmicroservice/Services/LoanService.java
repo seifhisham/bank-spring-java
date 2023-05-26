f4be31f47bd39c93abbb03d7ea320aa3cf1dd3ac
ahmedemad2222 <86380434+ahmedemad2222@users.noreply.github.com>
2023-05-26 20:19:44 +0300

    edit loan service

--- loan-microservice/src/main/java/com/loanmicroservice/Services/LoanService.java
+++ loan-microservice/src/main/java/com/loanmicroservice/Services/LoanService.java
@@ -1,12 +1,7 @@
 package com.loanmicroservice.Services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.loanmicroservice.loanmicroservice.Models.Loan;
-import com.loanmicroservice.loanmicroservice.Models.Loan.LoanStatus;
-
 import com.loanmicroservice.loanmicroservice.Repositories.LoanRepo;
 
 
