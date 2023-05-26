aba9243534cca12dd025706dc8ebd9b0a32a6694
Mohamed-Said12 <99090723+Mohamed-Said12@users.noreply.github.com>
2023-05-26 22:30:19 +0300

    Loan Controller in microservice

--- loan-microservice/src/main/java/com/loanmicroservice/loanmicroservice/Repositories/LoanRepo.java
+++ loan-microservice/src/main/java/com/loanmicroservice/loanmicroservice/Repositories/LoanRepo.java
@@ -5,10 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.loanmicroservice.loanmicroservice.Models.Loan;
 
-
 @Repository
-public interface LoanRepo extends JpaRepository<Loan, Integer> {
-
-
+public interface LoanRepo extends JpaRepository<Loan, Long> {
 
 }
